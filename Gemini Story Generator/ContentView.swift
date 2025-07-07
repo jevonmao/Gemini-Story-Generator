ab1b8721655df4602a814de7633ed2fcf7b29580
Jevon Mao <jevonmao@outlook.com>
2025-07-07 15:34:52 +0800

    Initial Commit

---
+++ Gemini Story Generator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gemini Story Generator
+//
+//  Created by Jevon Mao on 7/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gemini Story Generator/Gemini_Story_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gemini_Story_GeneratorApp.swift
+//  Gemini Story Generator
+//
+//  Created by Jevon Mao on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Gemini_Story_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
